1362e9282863ba02d85030cfae01cb929f5fbd96
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-09-16 14:29:29 -0400

    ADD: working version of HW4

--- Lesson4HW/app/src/main/java/cecelia/lesson3hw/MainActivityFragment.java
+++ Lesson4HW/app/src/main/java/cecelia/lesson3hw/MainActivityFragment.java
@@ -1,7 +1,5 @@
 package cecelia.lesson3hw;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.app.Fragment;
 import android.os.Bundle;
@@ -9,10 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,26 +27,28 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        //putting textViews in an array for easier looping
-        ArrayList<String> toDos = new ArrayList<String>();
-
+        ToDoAdapter todoAdapter = createToDoAdapter();
+        ListView listView = createListView(view, todoAdapter);
         Button addTodoButton = (Button) view.findViewById(R.id.add_todo_button);
 
-        final ToDoAdapter todoAdapter = new ToDoAdapter(getActivity(), toDos);
+        setOnClickListeners(listView, addTodoButton, todoAdapter);
 
-        final ListView listView = (ListView) view.findViewById(R.id.to_do_list);
-        listView.setAdapter(todoAdapter);
+        return view;
+    }
 
+    private void setOnClickListeners(ListView listView, Button addTodoButton, final ToDoAdapter todoAdapter) {
+        //when you click on list text, a alert dialog opens to edit the Todo
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                ArrayAdapter<String> adapter = (ArrayAdapter<String>) adapterView.getAdapter();
+                ToDoAdapter adapter = (ToDoAdapter) adapterView.getAdapter();
                 final String value = (String) adapterView.getItemAtPosition(position);
                 AlertDialog alert = createEditToDoAlertDialog("edit", (ToDoAdapter)adapter, position, value);
                 alert.show();
             }
         });
 
+        //the "+" in the corner opens a similar alert dialog for a brand new todo
         addTodoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,27 +56,76 @@ public class MainActivityFragment extends Fragment {
                 alert.show();
             }
         });
-        return view;
     }
 
-    private AlertDialog createEditToDoAlertDialog(final String action, final ToDoAdapter adapter, final int position, final String value) {
+    public AlertDialog createEditToDoAlertDialog(final String action, final ToDoAdapter adapter, final int position, final String value) {
         //used code from:
         // http://stackoverflow.com/questions/2115758/how-do-i-display-an-alert-dialog-on-android
         // http://stackoverflow.com/questions/13584063/adding-edittext-to-alert-dialog
 
-        //creating the alert dialog and setting the custom view to it
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = LayoutInflater.from(getActivity());
-        View layout = inflater.inflate(R.layout.todo_text_dialog, null);
-        builder.setView(layout);
-        builder.setCancelable(true);
+        //creating the alert dialog builder
+        AlertDialog.Builder builder = createBuilder();
+
+        //setting custom view to alert dialog
+        View layout = setBuilderCustomLayout(builder);
+
 
         //grabbing the input as final so that we can change from the listener
         final EditText todoInput = (EditText) layout.findViewById(R.id.todoInput);
+        initializeTextField(todoInput, value);
+
+        setDialogPositiveNegativeButtons(builder, todoInput, action, adapter, value, position);
+
+        return builder.create();
+    }
+
+    private void editToDo(String textInput, ToDoAdapter adapter, String value, int position) {
+        adapter.remove(value);
+        if (!textInput.isEmpty()) {
+            //remove the original item and insert the edited item in the same position
+            adapter.insert(textInput, position);
+        }
+    }
+
+    private void createToDo(String textInput, ToDoAdapter adapter) {
+        if (!textInput.isEmpty()) {
+            //remove the original item and insert the edited item in the same position
+            adapter.add(textInput);
+        }
+    }
+
+    private ToDoAdapter createToDoAdapter() {
+        ArrayList<String> toDos = new ArrayList<String>();
+        final ToDoAdapter todoAdapter = new ToDoAdapter(getActivity(), toDos);
+        return todoAdapter;
+    }
+
+    private ListView createListView(View view, ToDoAdapter todoAdapter) {
+        ListView listView = (ListView) view.findViewById(R.id.to_do_list);
+        listView.setAdapter(todoAdapter);
+        return listView;
+    }
+
+    private View setBuilderCustomLayout(AlertDialog.Builder builder) {
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View layout = inflater.inflate(R.layout.todo_text_dialog, null);
+        builder.setView(layout);
+        return layout;
+    }
+
+    private AlertDialog.Builder createBuilder() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(true);
+        return builder;
+    }
+
+    private void initializeTextField(TextView todoInput, String value) {
         if (!value.isEmpty()) {
             todoInput.setText(value);
         }
+    }
 
+    private void setDialogPositiveNegativeButtons(AlertDialog.Builder builder, final TextView todoInput, final String action, final ToDoAdapter adapter, final String value, final int position) {
         //setting the positive button to record the new to do unless no new to do was entered
         builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
             @Override
@@ -101,24 +147,5 @@ public class MainActivityFragment extends Fragment {
                 dialogInterface.cancel();
             }
         });
-
-        AlertDialog alert = builder.create();
-        return alert;
     }
-
-    public void editToDo(String textInput, ToDoAdapter adapter, String value, int position) {
-        adapter.remove(value);
-        if (!textInput.isEmpty()) {
-            //remove the original item and insert the edited item in the same position
-            adapter.insert(textInput, position);
-        }
-    }
-
-    public void createToDo(String textInput, ToDoAdapter adapter) {
-        if (!textInput.isEmpty()) {
-            //remove the original item and insert the edited item in the same position
-            adapter.add(textInput);
-        }
-    }
-
 }
--- Lesson4HW/app/src/main/java/cecelia/lesson3hw/ToDoAdapter.java
+++ Lesson4HW/app/src/main/java/cecelia/lesson3hw/ToDoAdapter.java
@@ -1,10 +1,12 @@
 package cecelia.lesson3hw;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,17 +21,56 @@ public class ToDoAdapter extends ArrayAdapter<String> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        String todo = getItem(position);
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final String todo = getItem(position);
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_todo, parent,false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_todo, parent, false);
         }
 
-        TextView item_todo = (TextView) convertView.findViewById(R.id.todo_item);
+        initializeToDo(convertView, todo);
+        ImageView deleteButton = (ImageView) convertView.findViewById(R.id.delete_button);
+        ImageView doneButton = initializeDoneButton(convertView);
 
-        item_todo.setText(todo);
+        setOnClickListeners(doneButton, deleteButton, todo);
 
         return convertView;
+    }
+
+    private void setOnClickListeners(ImageView doneButton, ImageView deleteButton, final String todo) {
+        //delete button removes to do from the list
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToDoAdapter.this.remove(todo);
+            }
+        });
+
+        //done button displays a bright green check mark next to item but item stays on screen
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if item is already checked (1), uncheck (0), and vice versa.
+                if ((int) view.getTag() == 0) {
+                    view.setTag(1);
+                    view.setBackgroundColor(Color.GREEN);
+                } else if ((int) view.getTag() == 1) {
+                    view.setTag(0);
+                    view.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
+    }
+
+    private ImageView initializeDoneButton(View convertView) {
+        ImageView doneButton = (ImageView) convertView.findViewById(R.id.done_button);
+        //tag button with 0 initially, meaning "not done" (1 means done)
+        doneButton.setTag(0);
+        return doneButton;
+    }
+
+    private void initializeToDo(View convertView, String todo) {
+        TextView itemTodo = (TextView) convertView.findViewById(R.id.todo_item);
+        itemTodo.setText(todo);
     }
 }
